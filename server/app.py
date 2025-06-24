21a8d9a5e51d17701b8209289d2a0484e54c38a5
anr.ron193@gmail.com <anr.ron193@gmail.com>
2025-06-24 17:21:35 +0300

    updates models and makes two routes

--- server/app.py
+++ server/app.py
@@ -21,7 +21,25 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake(id):
+    earthquake = Earthquake.query.get(id)
+    if earthquake:
+        return make_response(earthquake.to_dict(), 200)
+    return make_response({"message": f"Earthquake {id} not found."}, 404)
 
-
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def filter_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    if earthquakes:
+        return make_response({
+            "count": len(earthquakes),
+            "quakes": [earthquake.to_dict() for earthquake in earthquakes]
+            }, 200)
+    return make_response({
+        "count": 0,
+        "quakes": []
+    })
+    
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
